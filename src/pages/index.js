e76e26eeba79ee7c406008f1d2cc074eed8f2a19
Alexander Duria <alex.duria@outlook.com>
2021-03-16 22:46:25 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import Header from "../components/Header"
 import Hero from "../components/Hero"
 import Gallery from "../components/Gallery"
 import Services from "../components/Services"
